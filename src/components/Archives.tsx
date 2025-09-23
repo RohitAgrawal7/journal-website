932451d70ee3efde2d177fee0a46477d417d4cb8
RohitAgrawal7 <rohitmagrawal7@gmail.com>
2025-09-23 17:09:15 +0530

    add cover image

--- src/components/Archives.tsx
+++ src/components/Archives.tsx
@@ -177,7 +177,7 @@ const JournalIssueTOC = () => {
             <div className="flex flex-col md:flex-row gap-6 items-start">
               <div className="flex-shrink-0">
                 <img
-                  src="https://qtanalytics.in/journals/public/journals/5/cover_issue_259_en_US.jpg"
+                  src="./public/cover.png"
                   alt="Journal Cover Vol. 48 (2025)"
                   className="w-64 h-auto rounded-lg shadow-md border border-teal-200"
                 />
